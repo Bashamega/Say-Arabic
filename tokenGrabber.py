39dc4081f9de3a07fcb70dfd252977134a5c76e8
Bashamega <adambashaahmednaji@gmail.com>
2025-04-12 10:44:24 +0200

    update

--- tokenGrabber.py
+++ tokenGrabber.py
@@ -1,9 +1,13 @@
-# get_refresh_token.py
 from google_auth_oauthlib.flow import InstalledAppFlow
 
+# Set up OAuth flow
 flow = InstalledAppFlow.from_client_secrets_file(
     'client_secret.json',
     scopes=["https://www.googleapis.com/auth/youtube.upload"]
 )
-credentials = flow.run_console()
+
+# Open the authorization URL in the browser and let the user authenticate
+credentials = flow.run_local_server(port=0)
+
+# Output the refresh token
 print("Refresh token:", credentials.refresh_token)
